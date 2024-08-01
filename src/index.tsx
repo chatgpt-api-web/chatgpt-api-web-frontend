bc25706bf462ee62b3dc6344a577c0d031f3dd0e
SadPencil <sadpencil@outlook.com>
2024-08-02 02:58:16 +0800

    fix: do not register service worker on raw IPv4 hosts

--- src/index.tsx
+++ src/index.tsx
@@ -14,10 +14,22 @@ root.render(
     </React.StrictMode>
 );
 
+// Check if the website is hosted on an IPv4 address. If so, do not register service worker.
+const isHostedOnRawIPv4 = () => {
+    const hostname = window.location.hostname;
+    const isIPv4 = /^(\d+\.\d+\.\d+\.\d+)$/.test(hostname);
+    return isIPv4;
+};
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+if (isHostedOnRawIPv4()) {
+    serviceWorkerRegistration.unregister();
+} else {
+    serviceWorkerRegistration.register();
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
